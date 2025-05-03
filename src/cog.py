9427a4f3b2cd193677518c0b450c711599b5d636
yoshiki193 <maromu193@gmail.com>
2025-05-03 19:09:27 +0900

    "master"

--- src/cog.py
+++ src/cog.py
@@ -13,30 +13,26 @@ logging.basicConfig(
     ]
 )
 
-logger = logging.getLogger("discord")
+logger = logging.getLogger('discord')
 logger.setLevel(logging.INFO)
 
 class command(commands.Cog):
     def __init__(self, bot:commands.Bot):
-        with open('data.json') as f:
-            data = json.load(f)
-        self.last_message_id = data['lastMessageId']
-        self.send_channel_id = data['sendChannelId']
         self.bot = bot
-    
+
     async def generate_embed(self):
         with open('data.json') as f:
             data_list:dict = json.load(f)['list']
-            
+
         payload = []
-        
+
         for i in data_list:
             user:discord.User = await self.bot.fetch_user(i)
             payload.append({
                 'name':f'__**{user.display_name}\t{data_list[i]}\t#1**__' if data_list[i] == max(j for j in data_list.values()) else f'{user.display_name}\t{data_list[i]}',
                 'value':'█'*math.ceil((data_list[i]/max(j for j in data_list.values()))*10)
             })
-            
+
         embed = {
             'title':'**寝落ちカウンター**',
             'description':'コマンド：/add',
@@ -51,40 +47,35 @@ class command(commands.Cog):
 
     @commands.Cog.listener()
     async def on_message(self, message:discord.Message):
-        if (message.author !=  self.bot.user or "updated" in message.content) and message.channel.id == self.send_channel_id:
-            with open('data.json') as f:
-                data:dict = json.load(f)
-
-            if self.last_message_id == 0:
+        with open('data.json') as f:
+            data:dict = json.load(f)
+        if (message.author !=  self.bot.user or 'updated' in message.content) and message.channel.id == data['sendChannelId']:
+            if data['lastMessageId'] == 0:
                 send_message = await message.channel.send(embed = discord.Embed.from_dict(await self.generate_embed()))
-                self.last_message_id = send_message.id
+                data['lastMessageId'] = send_message.id
             else:
-                send_channel:discord.TextChannel = await self.bot.fetch_channel(self.send_channel_id)
-                last_message:discord.Message = await send_channel.fetch_message(self.last_message_id)
+                send_channel:discord.TextChannel = await self.bot.fetch_channel(data['sendChannelId'])
+                last_message:discord.Message = await send_channel.fetch_message(data['lastMessageId'])
                 await last_message.delete()
                 send_message = await message.channel.send(embed = discord.Embed.from_dict(await self.generate_embed()))
-                self.last_message_id = send_message.id
-            
-            data['lastMessageId'] = self.last_message_id
+                data['lastMessageId'] = send_message.id
 
             with open('data.json', 'w') as f:
                 json.dump(data, f, indent = 2)
-        elif message.author != self.bot.user and message.channel.id != self.send_channel_id:
+        elif message.author != self.bot.user and message.channel.id != data['sendChannelId']:
             with open('data.json') as f:
-                data:dict = json.load(f)              
+                data:dict = json.load(f)
             if str(message.channel.id) in [i for i in data['announcement']]:
                 last_message = await message.channel.fetch_message(data['announcement'][str(message.channel.id)]['messageId'])
                 await last_message.delete()
                 send_message = await message.channel.send(content=data['announcement'][str(message.channel.id)]['value'])
                 data['announcement'][str(message.channel.id)]['messageId'] = send_message.id
+
                 with open('data.json', 'w') as f:
                     json.dump(data, f, indent = 2)
 
-            
-
-    
     @discord.app_commands.command(
-        description = "add to counter"
+        description = 'add to counter'
     )
     async def add(self,interaction:discord.Interaction,member:discord.Member):
         with open('data.json') as f:
@@ -98,20 +89,19 @@ class command(commands.Cog):
             json.dump(data, f, indent = 2)
         
         await interaction.response.send_message(content=f'updated\t{member.display_name}\t{datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")}')
-    
+
     @discord.app_commands.command(
-        description = "announcement"
+        description = 'announcement'
     )
-    @discord.app_commands.describe(mode="モードの指定")
+    @discord.app_commands.describe(mode='モードの指定')
     @discord.app_commands.choices(mode=[
-        discord.app_commands.Choice(name="新規", value="new"),
-        discord.app_commands.Choice(name="削除", value="delete"),
+        discord.app_commands.Choice(name='新規', value='new'),
+        discord.app_commands.Choice(name='削除', value='delete'),
     ])
     async def announcement(self,interaction:discord.Interaction,mode:discord.app_commands.Choice[str],text:str = None):
-        if mode.value == "new" and interaction.channel_id != self.send_channel_id and text != None:
-            with open('data.json') as f:
-                data:dict = json.load(f)
-
+        with open('data.json') as f:
+            data:dict = json.load(f)
+        if mode.value == 'new' and interaction.channel_id != data['sendChannelId'] and text != None:
             data['announcement'][interaction.channel_id] = {}
             data['announcement'][interaction.channel_id]['value'] = text
             last_message = await interaction.response.send_message(content=text)
@@ -119,7 +109,7 @@ class command(commands.Cog):
 
             with open('data.json', 'w') as f:
                 json.dump(data, f, indent = 2)
-        elif mode.value == "delete" and interaction.channel_id != self.send_channel_id and text == None:
+        elif mode.value == 'delete' and interaction.channel_id != data['sendChannelId'] and text == None:
             with open('data.json') as f:
                 data:dict = json.load(f)
             
@@ -128,11 +118,12 @@ class command(commands.Cog):
 
                 with open('data.json', 'w') as f:
                     json.dump(data, f, indent = 2)
-                
+
                 await interaction.response.send_message(content='complete',silent=True)
             else:
                 await interaction.response.send_message(content='error',silent=True)
         else:
-            await interaction.response.send_message(content="error",silent=True)
+            await interaction.response.send_message(content='error',silent=True)
+
 async def setup(bot:commands.Bot):
     await bot.add_cog(command(bot))
